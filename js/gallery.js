2941db780d92a1c6d2882fcdaaf01fd2df991afd
adnan007d <adnanmansuri54@gmail.com>
2022-08-28 01:51:48 +0530

    gallery view done

---
+++ js/gallery.js
@@ -0,0 +1,74 @@
+const btn = document.getElementById("submit-btn");
+
+const apiUrl = "http://localhost:3000/api";
+
+const renderPosts = (posts) => {
+  let str = ``;
+  posts.map((post) => {
+    str += `<li class="product">
+        <div class="product-media">
+          <img
+            src="${post.imageLink}"
+            alt=""
+          />
+          <div class="wrapper-add-to-cart">
+            <div class="add-to-cart-inner">
+              <a
+                href="#"
+                id="${post._id}"
+                class="button product_type_simple add_to_cart_button ajax_add_to_cart octf-btn octf-btn-primary octf-btn-icon"
+                >Vote <i class="fa-solid fa-thumbs-up"></i
+              ></a>
+            </div>
+          </div>
+        </div>
+        <h2 class="woocommerce-loop-product__title">
+          <a href="#">${post.displayName}</a>
+        </h2>
+      </li>`;
+  });
+
+  return str;
+};
+
+btn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const cityElem = document.getElementById("city");
+  const categoryElem = document.getElementById("category");
+  const search = document.getElementById("search");
+  const start = 0;
+  const city = cityElem.options[cityElem.selectedIndex].value;
+  const category = categoryElem.options[categoryElem.selectedIndex].value;
+  const name = search.value;
+
+  const params = new URLSearchParams();
+  params.append("start", start);
+  if (city) {
+    params.append("city", city);
+  }
+  if (category) {
+    params.append("category", category);
+  }
+
+  if (name) {
+    params.append("name", name);
+  }
+
+  const res = await fetch(`${apiUrl}/posts?${params.toString()}`).then((res) =>
+    res.json()
+  );
+
+  document.querySelector(".products").innerHTML = renderPosts(res);
+});
+
+const fetchPosts = async () => {
+  const url = window.location.href;
+  const params = new URL(url).searchParams.toString();
+  const res = await fetch(`${apiUrl}/posts?${params.toString()}`).then((res) =>
+    res.json()
+  );
+  document.querySelector(".products").innerHTML = renderPosts(res);
+};
+
+window.onload = fetchPosts;
